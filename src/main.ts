f9e8fbcd81c6a5089ea9ac0ee41ba7125e997c88
Sergey <sergey1888888@gmail.com>
2021-03-13 20:14:39 +0300

    12

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.enableCors(); // protection
+  app.enableCors();
   await app.listen(process.env.PORT || '80');
 }
 bootstrap();
