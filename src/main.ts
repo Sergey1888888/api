27e52c69881e16f3c7c0cd3c15bc67ae7f4fe136
Sergey <sergey1888888@gmail.com>
2021-04-01 20:16:08 +0300

    cors fix?

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     }),
   );
   app.use({ origin: '*' });
-  await app.listen(process.env.PORT || '80');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
