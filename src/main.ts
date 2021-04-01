70dd9af8a8f4efb9dab9eab59dcc327fbae587fc
Sergey <sergey1888888@gmail.com>
2021-04-01 21:21:02 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: { origin: false } });
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Get,
   HttpCode,
   HttpStatus,
+  Headers,
   Param,
   Post,
   Put,
