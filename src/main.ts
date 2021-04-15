f12c8e8469f1df4856f61d7b8e6968c68505c1c5
Sergey <sergey1888888@gmail.com>
2021-04-15 23:47:47 +0300

    опять корс

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: '*' },
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    },
   });
   app.useGlobalPipes(
     new ValidationPipe({
