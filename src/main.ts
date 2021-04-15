b43e43221546f4062cad5eb20d8c6794bf31894a
Sergey <sergey1888888@gmail.com>
2021-04-16 01:10:09 +0300

    опять корс

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [/^http:\/\/localhost(?::[0-9]+)?$/],
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: '*' },
   });
   app.useGlobalPipes(
     new ValidationPipe({
