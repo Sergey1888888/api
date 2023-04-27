0d6820c1955354bbb3e632093547818d2a3bd242
Zaboronok Sergey <zaboronoksv@sushivesla.team>
2023-04-28 01:01:34 +0300

    vercel cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: /.+/ },
+    cors: {
+      origin: /.+/,
+      allowedHeaders: ['Accept', 'Content-Type'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    },
   });
   app.useGlobalPipes(
     new ValidationPipe({
